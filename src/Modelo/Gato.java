2b2f2f67c60ab9826f7ab063bc4480b32776b070
Nick_Celemin <nicolas.vargas@estudiantesunibague.edu.co>
2026-09-10 23:57:26 -0500

    Cambios

--- src/Modelo/Gato.java
+++ src/Modelo/Gato.java
@@ -3,4 +3,28 @@ import Modelo.Mascota;
 
 public class Gato extends Mascota {
 
+	
+	public Gato(String codigo, String nombre, int edad, double peso, String nombrePropietario) {
+		super(codigo, nombre, edad, peso, nombrePropietario);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTipo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String emitirSonido() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calcularCostoCOnsulta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
